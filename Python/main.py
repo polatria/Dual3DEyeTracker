80cc24527370007cede4ff04708c9a646b65f1b2
tmxdev <15144450+tmxdev@users.noreply.github.com>
2019-06-06 16:12:09 +0900

    Add FPS calculation

--- Python/main.py
+++ Python/main.py
@@ -40,11 +40,22 @@ if not devices:
 for i in range(len(devices)):
     if devices[i]['name'] == 'Pupil Cam1 ID0':
         cap[0] = uvc.Capture(devices[i]['uid'])
+        cap[0].frame_mode = (640, 480, 60)
     if devices[i]['name'] == 'Pupil Cam1 ID1':
         cap[1] = uvc.Capture(devices[i]['uid'])
+        cap[1].frame_mode = (640, 480, 60)
+
+print(cap[0].avaible_modes)
 
 # Eye detector init
 trk = EyeDetector.alloc()
+
+# for FPS calculation
+tm = cv2.TickMeter()
+tm.start()
+count = 0
+max_count = 100
+fps = 0
 
 # Main loop
 threading.Thread(target=key_check).start()
@@ -56,14 +67,24 @@ while is_run:
 
     # Fetch images
     for i in range(len(EYES)):
-        img[i] = np.array(cap[i].get_frame_robust().img)
+        img[i] = cap[i].get_frame_robust().img  # ndarray (cvMat)
 
     # Process each camera images
     for i in range(len(EYES)):
         EyeDetector.detect(trk, img[i], i, key)
+        # cv2.imshow(EYES[i], img[i])
 
-    cv2.waitKey(1)  # 1 msec
+    cv2.waitKey(1)  # Required to display camera image
 
     # Compute FPS
+    if count == max_count:
+        tm.stop()
+        fps = max_count / tm.getTimeSec()
+        print(f"{fps:.1f} fps")
+        tm.reset()
+        tm.start()
+        count = 0
+    count += 1
 
 cv2.destroyAllWindows()
+cap = None
